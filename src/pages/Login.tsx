81b467fb1d3f9951d34d8013839d9cdb63413e8f
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-02-05 17:02:11 +0545

    changes in login pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,48 +2,52 @@ import "./Login.css";
 import loginimg from "../assets/loginimg.png";
 import { NavLink } from "react-router-dom";
 import { LuHeartHandshake } from "react-icons/lu";
+import AuthLayout from "../components/common/AuthLayout";
+
 function Login() {
   return (
-    <>
-      <div className="login-container">
-        <div className="login-image-section">
-          <img src={loginimg} alt="Login" className="login-image" />
-        </div>
-        <div className="login-form-section">
-          <h2 className="login-title"><LuHeartHandshake />Welcome Back!</h2>
-          <form className="login-form">
-            <label htmlFor="email" className="login-label">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="login-input"
-              placeholder="Enter your email"
-              required
-            />
+    <AuthLayout
+      image={loginimg}
+      imageAlt="Login"
+      containerClassName="login-container"
+      formSectionClassName="login-form-section"
+      imageSectionClassName="login-image-section"
+      imageClassName="login-image"
+      title={<><LuHeartHandshake />Welcome Back!</>}
+      titleClassName="login-title"
+      formClassName="login-form-wrapper"
+    >
+      <form className="login-form">
+        <label htmlFor="email" className="login-label">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="login-input"
+          placeholder="Enter your email"
+          required
+        />
 
-            <label htmlFor="password" className="login-label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="login-input"
-              placeholder="Enter your password"
-              required
-            />
-          <br />
-            <button type="submit" className="login-submit-btn">
-              Login
-            </button>
-          </form>
-          <div className="register-text">
-            Don’t have an account? <NavLink to="/register"><span>Register</span></NavLink>
-          </div>
-        </div>
+        <label htmlFor="password" className="login-label">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="login-input"
+          placeholder="Enter your password"
+          required
+        />
+        <br />
+        <button type="submit" className="login-submit-btn">
+          Login
+        </button>
+      </form>
+      <div className="register-text">
+        Don’t have an account? <NavLink to="/register"><span>Register</span></NavLink>
       </div>
-    </>
+    </AuthLayout>
   );
 }
 
