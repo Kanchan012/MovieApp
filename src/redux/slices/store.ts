decc6ae863633527f59ff70d2a39ca974e37f33b
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-02-11 15:33:48 +0545

    added redux folder and include hook and store.ts

---
+++ src/redux/slices/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import moviesReducer from './slices/moviesSlice.ts';
+import watchlistReducer from './slices/watchlistSlice.ts';
+import reviewsReducer from './slices/reviewsSlice.ts';
+
+export const store = configureStore({
+    reducer: {
+        movies: moviesReducer,
+        watchlist: watchlistReducer,
+        reviews: reviewsReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
