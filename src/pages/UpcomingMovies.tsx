8771e4207fd213b8c5f6fc2c9caef41954eae812
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-02-11 16:44:55 +0545

    updated upcomingmovies file

--- src/pages/UpcomingMovies.tsx
+++ src/pages/UpcomingMovies.tsx
@@ -1,34 +1,19 @@
-import { useEffect, useState } from "react";
-import { fetchUpcomingMovies, type TrendingItem } from "../services/tmdbApi";
+import { useEffect } from "react";
+import { useMovies } from "../redux/useMovies";
 import MovieGrid from "../components/common/MovieGrid";
 import "./UpcomingMovies.css";
 
 function UpcomingMovies() {
-    const [movies, setMovies] = useState<TrendingItem[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+    const { upcoming, loading, error, getUpcoming } = useMovies();
 
     useEffect(() => {
-        const getUpcoming = async () => {
-            try {
-                setLoading(true);
-                const data = await fetchUpcomingMovies();
-                setMovies(data.results);
-            } catch (err) {
-                setError("Error loading upcoming movies.");
-                console.error(err);
-            } finally {
-                setLoading(false);
-            }
-        };
-
         getUpcoming();
     }, []);
 
     if (loading) {
         return (
             <div className="upcoming-loading">
-                <div className="spinner">Loading...</div>
+                <div className="spinner"></div>
             </div>
         );
     }
@@ -39,17 +24,17 @@ function UpcomingMovies() {
 
     return (
         <div className="upcoming-container">
-        <header className="upcoming-header">
-        <h1>Upcoming Movies</h1>
-      </header>
-        <MovieGrid
-            movies={movies}
-            containerClassName="upcoming-container"
-            gridClassName="upcoming-grid"
-            cardClassName="upcoming-card"
-            imageClassName="upcoming-poster"
-            infoClassName="upcoming-info"
-        />
+            <header className="upcoming-header">
+                <h1>Upcoming Movies</h1>
+            </header>
+            <MovieGrid
+                movies={upcoming}
+                containerClassName="upcoming-container"
+                gridClassName="upcoming-grid"
+                cardClassName="upcoming-card"
+                imageClassName="upcoming-poster"
+                infoClassName="upcoming-info"
+            />
         </div>
     );
 }
