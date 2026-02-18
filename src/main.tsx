9098af1602ac2b4a90b7364facfb628f1a2398fe
Kanchan Lawoju <lawojukanchan@gmail.com>
2026-02-18 16:29:46 +0545

    added persist in main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import './index.css'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+     </PersistGate>
   </Provider>
 );
